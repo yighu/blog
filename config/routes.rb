c24a38a71975dde1d250a5a3f1cbb7c2f3713a29
Yiguang Hu <yiguang.hu@gmail.com>
2011-12-19 17:19:23 -0500

    add more

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  resources :posts
+
   get "home/index"
 
   # The priority is based upon order of creation:
@@ -47,6 +49,10 @@ Blog::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #
+  resources :posts do
+	  resources :comments
+  end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
